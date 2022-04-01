bb292e95eb2cc96ba64731e9e4deac493c494236
JP <juanpablocapilla@gmail.com>
2022-04-01 14:09:47 -0300

    Primer commit JP

---
+++ src/helpers/validations.js
@@ -0,0 +1,62 @@
+export const validationRegister = values => {
+    let errors = {};
+  
+    if(!values.name) {
+      errors.name = 'El nombre es obligatorio';
+    } else if(values.name.length > 30) {
+      errors.name = 'El nombre no puede tener más de 30 caracteres';
+    }
+    if (!values.lastname) {
+      errors.lastname = 'El apellido es obligatorio';
+    } else if (values.lastname.length > 30) {
+      errors.lastname = 'El apellido no puede tener más de 30 caracteres';
+    }
+  
+    if (!values.email) {
+      errors.email = 'El email es obligatorio';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'El email no es válido';
+    } else if (values.email.length > 30) {
+      errors.email = 'El email no debe poseer más de 30 caracteres';
+    }
+  
+    if (!values.phone) {
+      errors.phone = 'El número de teléfono es obligatorio';
+    } else if (values.phone.length > 15) {
+      errors.phone = 'El número de teléfono no puede tener más de 15 caracteres';
+    }
+  
+    if (!values.password) {
+      errors.password = 'La contraseña es obligatoria'
+    } else if (values.password !== values.confirmPassword) {
+      errors.password = 'Las contraseñas no coinciden'
+    } else if (values.password.length < 8) {
+      errors.password = 'La contraseña debe tener como mínimo 8 caracteres'
+    } else if (values.password.length > 30) {
+      errors.password = 'La contraseña no debe poseer más de 30 caracteres'
+    }
+  
+  
+    return errors
+  }
+  
+  export const validationLogin = values => {
+    console.log(values);
+    let errors = {};
+    if (!values.email) {
+      errors.email = 'El email es obligatorio';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'El email no es válido';
+    } else if (values.email.length > 30) {
+      errors.email = 'El email no debe poseer más de 30 caracteres';
+    }
+  
+    if (!values.password) {
+      errors.password = 'La contraseña es obligatoria'
+    } else if (values.password.length < 8) {
+      errors.password = 'La contraseña debe tener como mínimo 8 caracteres'
+    } else if (values.password.length > 30) {
+      errors.password = 'La contraseña no debe poseer más de 30 caracteres'
+    }
+    return errors;
+  }
---
+++ src/hooks/useForm.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+const useForm = (initialState, validate, submit) => {
+  
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    if(submitting) {
+      if(Object.keys(errors).length === 0) {
+        submit();
+      }
+      setSubmitting(false);
+    }
+  }, [errors])
+  
+  const handleKeyUp = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const errorsValidate = validate(values);
+    setErrors(errorsValidate);
+    setSubmitting(true)
+  }
+
+  return {
+    values,
+    errors,
+    handleKeyUp,
+    handleSubmit
+  }
+
+}
+ 
+export default useForm;
