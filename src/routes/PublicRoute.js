ec02fce4cc3a2ab6a800da8afa2b6529a612b3d4
Matias Neira <matiasneirab@gmail.com>
2022-04-17 13:19:33 -0300

    agregue useEffect routes

--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -1,20 +1,15 @@
 import { useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
-import {UserContext} from "./../context/UserContext"
+import { UserContext } from "./../context/UserContext"
 
-const PublicRoute = ({children}) => {
-<<<<<<< HEAD
-    const {auth, getAuth} = useContext(UserContext);
-    useEffect(()=>{
-    getAuth();
-    },[])
-=======
-    const {auth, getAuth }= useContext(UserContext);
-    getAuth();
->>>>>>> 09f2031172a9bc161dc2351b55101d14f767c3ca
-    return(
-        !auth? children : <Navigate to='/home'/>
+const PublicRoute = ({ children }) => {
+    const { auth, getAuth } = useContext(UserContext);
+    useEffect(() => {
+        getAuth();
+    }, []);
+    return (
+        !auth ? children : <Navigate to='/home' />
     );
-    }
+}
 
 export default PublicRoute
