f5313282daee3e2901491cc7c915a14239a063a4
Juan <guaca33@gmail.com>
2022-04-19 20:22:06 -0300

    Validacion en register

--- src/helpers/validationRegister.js
+++ src/helpers/validationRegister.js
@@ -4,17 +4,17 @@ export const validationRegister = values => {
     if (!values.name) {
       errors.password = 'El nombre es obligatorio'
     } else if (values.name.length < 3) {
-      errors.name = 'La contraseña debe tener como mínimo 8 caracteres'
+      errors.name = 'El nombre debe tener como mínimo 3 caracteres'
     } else if (values.name.length > 30) {
-      errors.name = 'La contraseña no debe poseer más de 30 caracteres'
+      errors.name = 'El nombre no debe poseer más de 30 caracteres'
     }
   
     if (!values.lastname) {
       errors.lastname = 'El apellido es obligatorio'
     } else if (values.lastname.length < 3) {
-      errors.lastname = 'La contraseña debe tener como mínimo 8 caracteres'
+      errors.lastname = 'El apellido debe tener como mínimo 3 caracteres'
     } else if (values.lastname.length > 30) {
-      errors.lastname = 'La contraseña no debe poseer más de 30 caracteres'
+      errors.lastname = 'El apellido no debe poseer más de 30 caracteres'
     }
   
     if (!values.email) {
@@ -27,7 +27,7 @@ export const validationRegister = values => {
   
     if (!values.password) {
       errors.password = 'La contraseña es obligatoria'
-    } else if (values.password.length < 3) {
+    } else if (values.password.length < 8) {
       errors.password = 'La contraseña debe tener como mínimo 8 caracteres'
     } else if (values.password.length > 30) {
       errors.password = 'La contraseña no debe poseer más de 30 caracteres'
