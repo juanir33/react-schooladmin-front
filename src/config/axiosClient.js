6c204f0cce6dd4b6205f1413379ac6d71fa8f00a
juanir33 <guaca33@gmail.com>
2022-08-23 20:08:09 -0300

    cambio baseURL en axios

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://217.71.204.219:4000/api/v1'
+  baseURL: 'https://217.71.204.219:4000/api/v1'
 })
 
 export default axiosClient
